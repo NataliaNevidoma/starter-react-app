57e3a4e9ca3aab4b7672d7407bfd98203caf067d
NataliaNevidoma <56003837+NataliaNevidoma@users.noreply.github.com>
2023-07-31 22:02:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my training app on Cyclic by Natalia Nevidoma
         </p>
         <a
           className="App-link"
